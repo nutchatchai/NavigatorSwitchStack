2c18743c0d782c4e7e0facf85ccb1018d3e7beb3
nutchatchai <53834514+nutchatchai@users.noreply.github.com>
2019-08-28 16:17:58 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+
+import  Page1  from "./stack/Page1";
+import  Page2  from "./stack/Page2";
+import  Page3  from "./stack/Page3";
+
+import { createStackNavigator, createAppContainer, createSwitchNavigator} from "react-navigation";
+
+const AppNavigator = createStackNavigator(
+  {
+    Page1: Page1,
+    Page2: Page2,
+    Page3: Page3
+   
+  }
+)
+
+const App2Navigator = createSwitchNavigator(
+  {
+    Page1: Page1,
+    Page2: Page2,
+    Page3: Page3
+   
+  }
+)
+const App = createAppContainer(App2Navigator);
+export default App;
